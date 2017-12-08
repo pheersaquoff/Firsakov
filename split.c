c1b923e3ba621ab20c4adc516981f39198a25dde
Nikita <pheersaquoff@gmail.com>
2017-12-08 09:27:16 +0300

    fixed

--- split.c
+++ split.c
@@ -10,7 +10,7 @@ void Split(char* string, char* delimiters, char*** tokensPtr, int* tokensCountPt
 	tokens[tokensCount] = (char*) calloc (maxLength, sizeof (char));
         tokens[tokensCount] = strtok (string, delimiters);
 	while (tokens[tokensCount] != NULL) {
-		printf("%d: %s\n", (tokensCount + 1), tokens[tokensCount]);
+		//printf("%d: %s\n", (tokensCount + 1), tokens[tokensCount]);
 		tokensCount++;
 		tokens[tokensCount] = (char*) calloc (maxLength, sizeof(char));
 		tokens[tokensCount] = strtok (NULL, delimiters);
@@ -30,7 +30,9 @@ int main () {
 	fgets (string, maxLetters, stdin);
 	printf ("Enter the delimiter: ");
         fgets (delimiters, maxLength, stdin);
-        Split (string, delimiters, &tokens, &tokensCount);   
+        Split (string, delimiters, &tokens, &tokensCount);  
+	for ( int i = 0; i < (tokensCount); i++) 
+		printf ("%d: %s\n", i + 1, tokens[i]); 
         free(tokens);
 }
 
